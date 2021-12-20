922bba391c196a069991ab18d14bde34a86d090f
user <PositiveWay05@gmail.com>
2021-12-20 18:49:03 +0400

    extended layout

--- build/dev.sh
+++ build/dev.sh
@@ -12,11 +12,15 @@ sudo /home/user/CLionProjects/evsieve/target/release/evsieve --input /dev/input/
         --map key:z@dev_layout key:leftctrl key:z \
         --map key:y@dev_layout key:leftctrl key:y \
         --map key:f@dev_layout key:leftctrl key:f \
+        --map key:s@dev_layout key:leftctrl key:s \
         --map key:r@dev_layout key:leftalt key:tab \
         --map key:w@dev_layout key:leftctrl key:w \
         --map key:t@dev_layout key:leftctrl key:t \
+        --map key:4@dev_layout key:leftctrl key:leftalt key:b \
+        --map key:5@dev_layout key:leftalt key:leftshift key:left \
+        --map key:space@dev_layout key:leftalt key:enter \
+        --map key:slash@dev_layout key:leftctrl key:slash \
         --map key:g@dev_layout key:leftshift key:f6 \
         --map key:d@dev_layout key:leftctrl key:d \
         --map key:l@dev_layout key:leftctrl key:leftalt key:l \
-        --map key:b@dev_layout key:leftctrl key:leftalt key:b \
         --output
